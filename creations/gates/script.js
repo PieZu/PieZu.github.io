a36f33f6326ce37fdc54a97972ed53a0f4b099b7
PieZu <39189047+PieZu@users.noreply.github.com>
2020-11-25 00:50:46 +1300

    detect mouseover

--- creations/gates/script.js
+++ creations/gates/script.js
@@ -11,21 +11,21 @@ var FONT_HEIGHT = 20
 const PALLETE = {
 	background: "#333",
 	sidebar: {
-        fontFamily: "sans-serif",
+		fontFamily: "sans-serif",
 		background: "#202020",
 		block: "#444",
-        text: "#fff"
+		text: "#fff"
 	},
-    topbar: {
-        background: "#272727",
-        text: "#fff",
-        font: "24px sans-serif"
-    },
-    playbar: {
-        background: "#111",
-        fontSize: 30,
-        fontFamily: "sans-serif"
-    }
+	topbar: {
+		background: "#272727",
+		text: "#fff",
+		font: "24px sans-serif"
+	},
+	playbar: {
+		background: "#111",
+		fontSize: 30,
+		fontFamily: "sans-serif"
+	}
 }
 ctx.font = FONT_HEIGHT+"px sans-serif"
 
@@ -51,70 +51,92 @@ var interactive_zones = []
 function render() {
 	// reset
 	ctx.clearRect(0,0, canvas.width, canvas.height)
-    interactive_zones = []
+	interactive_zones = []
 
-    // top bar thing
-    ctx.font = PALLETE.topbar.font
-    ctx.fillStyle = PALLETE.topbar.background
-    ctx.fillRect(0,0,canvas.width,50)
-    ctx.fillStyle = PALLETE.topbar.text
-    ctx.fillText(customBlocks[editing].name, 250 + 20, 33)
+	// top bar thing
+	ctx.font = PALLETE.topbar.font
+	ctx.fillStyle = PALLETE.topbar.background
+	ctx.fillRect(0,0,canvas.width,50)
+	ctx.fillStyle = PALLETE.topbar.text
+	ctx.fillText(customBlocks[editing].name, 250 + 20, 33)
 
-    interactive_zones.push([250+20-5, 5, ctx.measureText(customBlocks[editing].name).width+10, 50-10, "rename"])
+	interactive_zones.push([250+20-5, 5, ctx.measureText(customBlocks[editing].name).width+10, 50-10, "rename"])
 
 	// sidebar
 	ctx.fillStyle = PALLETE.sidebar.background
 	ctx.fillRect(0,0,250,canvas.height)
 
-    ctx.font = `${FONT_HEIGHT}px ${PALLETE.sidebar.fontFamily}`
+	ctx.font = `${FONT_HEIGHT}px ${PALLETE.sidebar.fontFamily}`
 	customBlocks.forEach(renderCustom)
 
-    // toolbox bar thing
-    ctx.fillStyle = PALLETE.playbar.background
-    ctx.fillRect(0,0,250,50)
+	// toolbox bar thing
+	ctx.fillStyle = PALLETE.playbar.background
+	ctx.fillRect(0,0,250,50)
 
-    ctx.font = `${PALLETE.playbar.fontSize}px ${PALLETE.playbar.fontFamily}`
-    ctx.fillStyle = "white"
-    ctx.fillText("▶⏸"[playing*1],15,35)
-    interactive_zones.push([15 -3,10 -3, PALLETE.playbar.fontSize+6, PALLETE.playbar.fontSize+6, "toggleplay"])
+	ctx.font = `${PALLETE.playbar.fontSize}px ${PALLETE.playbar.fontFamily}`
+	ctx.fillStyle = "white"
+	ctx.fillText("▶⏸"[playing*1],15,35)
+	interactive_zones.push([15 -3,10 -3, PALLETE.playbar.fontSize+6, PALLETE.playbar.fontSize+6, "toggleplay"])
 
-    for (i=0; i<3; i++) {
-        let x = 240-(i+1)*(PALLETE.playbar.fontSize+10)+5
-        ctx.fillText(['❌','💾','➕'][i], x,35)
-        interactive_zones.push([x+5-3, 10-3, PALLETE.playbar.fontSize+6, PALLETE.playbar.fontSize+6, ["clear", "savenew", "createnew"][i]])
+	for (let i=0; i<3; i++) {
+		let x = 240-(i+1)*(PALLETE.playbar.fontSize+10)+5
+		ctx.fillText(['❌','💾','➕'][i], x,35)
+		interactive_zones.push([x+5-3, 10-3, PALLETE.playbar.fontSize+6, PALLETE.playbar.fontSize+6, ["clear", "savenew", "createnew"][i]])
 
-    }
+	}
 
 
-    ctx.strokeStyle = "red"
-    interactive_zones.forEach(([x,y,w,h])=>{
-        ctx.strokeRect(x,y,w,h)
-    })
+	ctx.strokeStyle = "red"
+	interactive_zones.forEach(([x,y,w,h],i)=>{
+		ctx.strokeStyle = i==mouseover?"green":"red"
+		ctx.strokeRect(x,y,w,h)
+	})
 }
 
 SAVED_HEIGHT = 50
 SAVED_PADDING = 10
 SCROLLBAR_WIDTH = 5
 function renderCustom({name}, i) {
-    let width = Math.min(ctx.measureText(name).width+SAVED_PADDING+35, 250-SAVED_PADDING*2-SCROLLBAR_WIDTH)
+	let width = Math.min(ctx.measureText(name).width+SAVED_PADDING+35, 250-SAVED_PADDING*2-SCROLLBAR_WIDTH)
 
 	ctx.fillStyle = PALLETE.sidebar.block
-    let y_position = Math.round(i*SAVED_HEIGHT)-sidebarScroll +SAVED_PADDING,
-    dimensions = [SAVED_PADDING, y_position, width, SAVED_HEIGHT-SAVED_PADDING]
+	let y_position = Math.round(i*SAVED_HEIGHT)-sidebarScroll +SAVED_PADDING,
+	dimensions = [SAVED_PADDING, y_position, width, SAVED_HEIGHT-SAVED_PADDING]
 	ctx.roundRect(...dimensions, 9)
-    interactive_zones.push([...dimensions, "dragcustom", i])
+	interactive_zones.push([...dimensions, "dragcustom", i])
 
-    let text_y = Math.round(y_position + (SAVED_HEIGHT-SAVED_PADDING)/2 + FONT_HEIGHT/3)
-    ctx.fillStyle = PALLETE.sidebar.text
-    ctx.fillText(name, SAVED_PADDING*2, text_y, width-SAVED_PADDING-35)
+	let text_y = Math.round(y_position + (SAVED_HEIGHT-SAVED_PADDING)/2 + FONT_HEIGHT/3)
+	ctx.fillStyle = PALLETE.sidebar.text
+	ctx.fillText(name, SAVED_PADDING*2, text_y, width-SAVED_PADDING-35)
 
-    ctx.fillText("✎", SAVED_PADDING*2 + width-(0.8*SAVED_HEIGHT), text_y)
-    interactive_zones.push([SAVED_PADDING*2 + width-(0.8*SAVED_HEIGHT) -2, y_position+FONT_HEIGHT/2 -2, 20 +4, 20 +4, "edit", i])
+	ctx.fillText("✎", SAVED_PADDING*2 + width-(0.8*SAVED_HEIGHT), text_y)
+	interactive_zones.push([SAVED_PADDING*2 + width-(0.8*SAVED_HEIGHT) -2, y_position+FONT_HEIGHT/2 -2, 20 +4, 20 +4, "edit", i])
 }
 
 function renderBlock(x, y, colour) {
 	ctx.fillStyle = colour || "red"
 	ctx.fillRect(x,y, 100, 200)
+}
+
+mouse = {x:0, y:0}
+mouseover = -1
+canvas.onmousemove = (e) => {
+	mouse.x = e.clientX
+	mouse.y = e.clientY
+
+	mouseover = -1
+	for (var i=0; i<interactive_zones.length; i++) {
+		let zone = interactive_zones[i]
+		if (contains(zone, mouse)) {
+			mouseover = i
+			//ctx.fillStyle = "blue"
+			//ctx.fillRect(zone[0], zone[1], zone[2], zone[3])
+		}
+	}
+}
+
+function contains([sx,sy,w,h], {x,y}) {
+	return (sx<x && x<sx+w && sy<y && y<sy+h)
 }
 
 customBlocks = [{name: "== MAIN =="}, {name: "AND"}, {name: "NOT"}, {name: "4 BIT ADDER"}, {name: "SOME REALLY REALLY LONG NAME"}, {name: "."}]
